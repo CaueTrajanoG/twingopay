e35df50b4f1037775b1c6222234eb25ac6d43856
cauetrajanog <caue.sw@gmail.com>
2026-08-30 18:26:43 -0300

    Estrutura inicial do projeto TwingoPay

---
+++ src/main/java/br/edu/ifpb/pweb2/twingopay/TwingopayApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.twingopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwingopayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwingopayApplication.class, args);
+	}
+
+}
